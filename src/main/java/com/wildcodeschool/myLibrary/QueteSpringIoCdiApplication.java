791911c8e7704a9e6959dcd1f4c716a0b2603ec6
bigmoletos <bigmoletos@yopmail.com>
2019-10-13 18:35:23 +0200

    first commit

---
+++ src/main/java/com/wildcodeschool/myLibrary/QueteSpringIoCdiApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.myLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueteSpringIoCdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueteSpringIoCdiApplication.class, args);
+	}
+
+}
